267cffbc393bed7f42a79991c738889dde8fe994
Eric Bonta <eric.bonta@hotmail.com>
2021-03-24 12:00:11 +0100

    Schema tillagt

---
+++ Bananfarm Schema/banansorter insert.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM bananfarm.bananasort
+LIMIT 0, 1000
+
+-- Date: 2021-03-24 11:46
+*/
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (1,'Yellow Ladie');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (2,'Sweet Steve');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (3,'Tiny Tim');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (4,'Long John Vegas');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (5,'Skinny Muppet');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (6,'Musa Bakeri');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (7,'Musa Boman');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (8,'Musa Hirta');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (9,'Musa Voonii');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (10,'Musa Rubra');
+INSERT INTO `` (`bananasort_id`,`banana_name`) VALUES (11,'Musa Zaifu');
